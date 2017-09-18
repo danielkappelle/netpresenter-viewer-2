15f88bd1ca592c6026b59e52b56d0c4b3592d54d
Daniel Kappelle <daniel.kappelle@gmail.com>
2017-09-18 10:20:50 +0200

    First version

---
+++ js/controller.js
@@ -0,0 +1,22 @@
+angular.module('netpresenter', [])
+  .controller('NetpresenterController', function($scope, $http, $timeout) {
+    // $scope.url = 'http://netpresenter.tudelft.nl/netpresenter/published/ETV_computers/channel/PC/';
+    $scope.url = 'http://netpresenter.tudelft.nl/netpresenter/published/3mE_algemeen/channel/PC/';
+    $scope.currentSlide = 0;
+    $scope.timeOut = 5000;
+
+    $http.get('php/api.php', {params: {url: $scope.url}})
+        .then(function(response) {
+          $scope.slides = response.data;
+          n_slides = $scope.slides.length;
+          startShow();
+        }, function(err) {
+          console.log(err);
+        });
+
+    var startShow = function() {
+      $scope.currentSlide = ($scope.currentSlide + 1) % n_slides;
+      $timeout(startShow, $scope.timeOut);
+    }
+
+  });
---
+++ php/api.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_GET['url'])) {
+  $matches = [];
+  $html = file_get_contents($_GET['url']);
+  preg_match_all("/>(\S+\.jpg)</", $html, $matches);
+  $matches = $matches[1];
+  echo json_encode($matches);
+  die();
+} else {
+  $response = ['err' => 'No URL specified'];
+  echo json_encode($response);
+  die();
+}
+
+?>
