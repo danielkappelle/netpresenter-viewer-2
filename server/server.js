7c046377859ad38722ede8dc9468c4123df19a80
Daniel Kappelle <daniel.kappelle@gmail.com>
2017-10-09 22:29:44 +0200

    Add OPTIONS method, necessary for HAproxy to work and allow proxies

--- server/server.js
+++ server/server.js
@@ -55,4 +55,12 @@ app.listen(3000, function() {
   console.log('Listening on 3000');
 });
 
+app.enable('trust proxy');
+
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
 
